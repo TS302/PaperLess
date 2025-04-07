5190b050a2b00d19021156659dfa4aa357a9d8af
Oguzhan Cirpan <ocirpan920@gmail.com>
2025-04-07 19:32:17 +0200

    07.04.2025

--- PaperLess/ContentView.swift
+++ PaperLess/ContentView.swift
@@ -5,37 +5,38 @@
 //  Created by Tom Salih on 28.03.25.
 //
 
+
 import SwiftUI
 
-struct ContentView: View {
+struct RegistrationView: View {
     
-   
     @State private var firstname = ""
     @State private var lastname = ""
     @State private var email = ""
-    @State private var password = ""
-    @State private var validatePassword = ""
-    @State private var showPassword: Bool = false
+    
+    @State private var password: String = "Passwort1."
+    @State private var confirmPassword: String = "Passwort1."
     @State private var errorMessage: String = ""
-    
-    
-    
-    
+    @State private var showPassword: Bool = false
     
     var body: some View {
         
         HStack {
-            Image ("Wolke1")
+            Image("Wolke1")
                 .resizable()
                 .frame(width: 100, height: 60)
             
             Text("Paperless")
                 .foregroundColor(.schwarz)
                 .padding(.leading, -30)
-                .font(.system(size:17))
+                .font(.system(size: 17))
         }
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding(.leading, -20)
+        
+        
+        
+        
         
         
         GeometryReader { geometry in
@@ -45,15 +46,23 @@ struct ContentView: View {
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .padding(.top, geometry.safeAreaInsets.top + 20)
+               
                 
                 
-                Spacer()
+                
+                
+                
+                Spacer().frame(height: 80)
+
+                
+                
+                
+                
                 
                 VStack(alignment: .leading, spacing: 5) {
                     Text("Vorname")
                         .font(.caption)
                         .foregroundColor(.grau)
-                    
                     
                     TextField("Max", text: $firstname)
                         .foregroundColor(.grau)
@@ -64,12 +73,18 @@ struct ContentView: View {
                             RoundedRectangle(cornerRadius: 8)
                                 .stroke(Color.blau, lineWidth: 1))
                 }
+               
+                
+                
+                
+                
                 
                 
                 VStack(alignment: .leading, spacing: 5) {
                     Text("Nachname")
                         .font(.caption)
                         .foregroundColor(.grau)
+                    
                     TextField("Mustermann", text: $lastname)
                         .foregroundColor(.grau)
                         .multilineTextAlignment(.center)
@@ -78,14 +93,20 @@ struct ContentView: View {
                         .background(
                             RoundedRectangle(cornerRadius: 8)
                                 .stroke(Color.blau, lineWidth: 1))
-                    
-                    
                 }
+                
+                
+                
+                
+                
+                
+                
                 
                 VStack(alignment: .leading, spacing: 5) {
                     Text("E-mail")
                         .font(.caption)
                         .foregroundColor(.grau)
+                    
                     TextField("max.mustermann@", text: $email)
                         .foregroundColor(.grau)
                         .multilineTextAlignment(.center)
@@ -95,109 +116,139 @@ struct ContentView: View {
                             RoundedRectangle(cornerRadius: 8)
                                 .stroke(Color.blau, lineWidth: 1))
                 }
+               
                 
                 
                 
                 
                 
-                VStack(alignment: .leading, spacing: 5) {
+                
+                
+                
+                
+                VStack(alignment: .leading) {
                     Text("Passwort")
                         .font(.caption)
                         .foregroundColor(.grau)
                     
-                    
                     HStack {
+                        if showPassword {
+                            TextField("Passwort", text: $password)
+                        } else {
+                            SecureField("Passwort", text: $password)
+                        }
                         
-                        SecureField("********", text: $password)
-                            .foregroundColor(.grau)
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .frame(width: 300, height: 44)
-                            .background(
-                                RoundedRectangle(cornerRadius: 8)
-                                    .stroke(Color.blau, lineWidth: 1))
-                        
-                        
+                        Button(action: {
+                            showPassword.toggle()
+                        }) {
+                            Image(systemName: showPassword ? "eye.slash" : "eye")
+                                .foregroundColor(.grau)
+                        }
                     }
-                    
-                }
-                
-                
-                
-                
-                
-                VStack(alignment: .leading, spacing: 5) {
-                    Text("Passwort bestätigen")
-                        .font(.caption)
-                        .foregroundColor(.grau)
-                    
-                    
-                    SecureField("********", text:
-                                    $validatePassword)
-                    .foregroundColor(.grau)
-                    .multilineTextAlignment(.center)
                     .padding()
                     .frame(width: 300, height: 44)
                     .background(
                         RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.blau, lineWidth: 1))
-                }
-                
-                
-                Spacer()
-                
-                
-                
-                
-                Button("Registrieren") {
-                    if PaperLess.validatePassword(password) {
-                        if password == validatePassword {
-                            errorMessage = "Registrierung erfolgreich!"
+                            .stroke(Color.blau, lineWidth: 1)
+                    )
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    Text("Passwort bestätigen")
+                        .font(.caption)
+                        .foregroundColor(.grau)
+                    
+                    HStack {
+                        if showPassword {
+                            TextField("Passwort bestätigen", text: $confirmPassword)
                         } else {
-                            errorMessage = "Passwörter stimmen nicht überein!"
+                            SecureField("Passwort bestätigen", text: $confirmPassword)
                         }
-                    } else {
-                        errorMessage = "Passwort muss mindestens 8 Zeichen enthalten!"
+                        
+                        Button(action: {
+                            showPassword.toggle()
+                        }) {
+                            
+                        }
+                    }
+                    .padding()
+                    .frame(width: 300, height: 44)
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.blau, lineWidth: 1)
+                    )
+                    
+                    
+                    
+                    
+                    
+                    Spacer()
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    VStack {
+                        Button("Registrieren") {
+                            if validatePassword(password) {
+                                if password == confirmPassword {
+                                    errorMessage = "Registrierung erfolgreich!"
+                                } else {
+                                    errorMessage = "Passwort stimmt nicht überein!"
+                                }
+                            } else {
+                                errorMessage = "Passwort muss 8 Zeichen enthalten!"
+                            }
+                        }
+                        .font(.system(size: 20))
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blau)
+                        .cornerRadius(10)
+                        
+                        Text(errorMessage)
+                            .foregroundColor(errorMessage.contains("erfolgreich") ? .green : .red)
+                            
+                    }
+                    
+                    
+                    
+                    HStack {
+                        Text("Hast du schon ein Konto?")
+                            .foregroundColor(.schwarz)
+                            .font(.system(size: 13))
+                        
+                        
+                        Button("Anmelden") {
+                        }
+                        .foregroundColor(.blau)
+                        .font(.system(size: 17))
                     }
                 }
-                .font(.system(size: 20))
-                .foregroundColor(.white)
-                .padding()
-                .background(Color.blue)
-                .cornerRadius(10)
-                
-                
-                
-               
-                HStack {
-                    Button("Hast du schon ein Konto?") {
-                    }
-                    .foregroundColor(.schwarz)
-                    .font(.system(size:13))
-                    
-                    Button("Anmelden") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                    }
-                    .foregroundColor(.blau)
-                    .font(.system(size:17))
-                    
-                    
-                    
-                }
-                
-                
             }
             .padding()
-            
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             
-            
-            
         }
-        
     }
 }
 
+
+
+
+func validatePassword(_ password: String) -> Bool {
+    let pattern = #"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$"#
+    return NSPredicate(format: "SELF MATCHES %@", pattern).evaluate(with: password)
+}
+
 #Preview {
-    ContentView()
+    RegistrationView()
 }
