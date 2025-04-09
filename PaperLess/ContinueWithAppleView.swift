1bbca6c1785b31e18b276850ce613db1a4f21b81
Oguzhan Cirpan <ocirpan920@gmail.com>
2025-04-09 04:50:39 +0200

    Code überarbeitet: Anmeldung mit Apple und Google hinzugefügt, Google-Logo implementiert

---
+++ PaperLess/ContinueWithAppleView.swift
@@ -0,0 +1,18 @@
+//
+//  ContinueWithAppleView.swift
+//  PaperLess
+//
+//  Created by Oguzhan Cirpan on 09.04.25.
+//
+
+import SwiftUI
+
+struct ContinueWithAppleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContinueWithAppleView()
+}
---
+++ PaperLess/ContinueWithGoogleView.swift
@@ -0,0 +1,18 @@
+//
+//  ContinueWithGoogleView.swift
+//  PaperLess
+//
+//  Created by Oguzhan Cirpan on 09.04.25.
+//
+
+import SwiftUI
+
+struct ContinueWithGoogleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContinueWithGoogleView()
+}
