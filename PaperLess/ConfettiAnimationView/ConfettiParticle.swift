676941f3366f087c8abc3a57927c262db2121dc1
Oguzhan Cirpan <ocirpan920@gmail.com>
2025-04-11 03:27:37 +0200

    Nach einer erfolgreichen Registrierung wird für kurze Zeit ein Konfetti-Effekt angezeigt, um den Erfolg zu feiern. Dieser Effekt bleibt für etwa 2-3 Sekunden sichtbar, bevor der Benutzer automatisch zum Home-Bildschirm weitergeleitet wird.

---
+++ PaperLess/ConfettiAnimationView/ConfettiParticle.swift
@@ -0,0 +1,8 @@
+//
+//  ConfettiParticle.swift
+//  PaperLess
+//
+//  Created by Oguzhan Cirpan on 11.04.25.
+//
+
+import Foundation
---
+++ PaperLess/Models/ConfettiAnimationView/ConfettiShape.swift
@@ -0,0 +1,8 @@
+//
+//  ConfettiShape.swift
+//  PaperLess
+//
+//  Created by Oguzhan Cirpan on 11.04.25.
+//
+
+import Foundation
