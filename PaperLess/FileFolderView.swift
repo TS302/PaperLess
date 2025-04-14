a121e2dd0b863bd249b0f843b2a46a2b584f1cbd
Oguzhan Cirpan <ocirpan920@gmail.com>
2025-04-14 20:34:26 +0200

    CodeProblem

---
+++ PaperLess/FileFolderView.swift
@@ -0,0 +1,18 @@
+//
+//  FileFolderView.swift
+//  PaperLess
+//
+//  Created by Oguzhan Cirpan on 13.04.25.
+//
+
+import SwiftUI
+
+struct FileFolderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FileFolderView()
+}
