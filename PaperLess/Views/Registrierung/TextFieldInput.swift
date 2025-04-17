417520f1701d2eaef4b546d11b92b65251e19d16
Oguzhan Cirpan <ocirpan920@gmail.com>
2025-04-17 16:52:12 +0200

    no message

--- PaperLess/Views/Registrierung/TextFieldInput.swift
+++ PaperLess/Views/Registrierung/TextFieldInput.swift
@@ -8,25 +8,36 @@
 import SwiftUI
 
 struct TextFieldInput: View {
+    
     var label: String
     @Binding var text: String
     
     var body: some View {
+        
         VStack(alignment: .leading, spacing: 5) {
-            Text(label)
-                .font(.caption)
-                .foregroundColor(.gray)
+            
             
             TextField(label, text: $text)
+            
                 .padding()
                 .frame(width: 300, height: 44)
-                .background(RoundedRectangle(cornerRadius: 8).fill(Color.white).stroke(Color.blue, lineWidth: 1))
+                .background(RoundedRectangle(cornerRadius: 8)
+                    .fill(Color.white)
+                    .stroke(Color.blue, lineWidth: 1))
         }
+        
+        
+        
+        
+        
+//        Hintergrundfarbe
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.hellblau)
     }
 }
 
 struct TextFieldInput_Previews: PreviewProvider {
     static var previews: some View {
-        TextFieldInput(label: "E-Mail", text: .constant(""))
+        TextFieldInput(label: "Text", text: .constant(""))
     }
 }
